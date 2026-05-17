da88f19699cc25cb505e91390ac88507eb864002
Aniaualt <achyzy@student.agh.edu.pl>
2026-05-17 17:27:05 +0200

    zmiany w main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,32 @@
 import './style.css';
 import dayjs from 'dayjs';
+
+const form = document.querySelector('#birthday-form');
+const dateInput = document.querySelector('#bday');
+const dialog = document.querySelector('#result-dialog');
+const closeBtn = document.querySelector('#close-dialog');
+const dialogContent = document.querySelector('#dialog-content');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+
+ 
+  const birthDate = dayjs(dateInput.value);
+  const today = dayjs();
+
+  const daysPassed = today.diff(birthDate, 'days');
+
+  const isBirthdayToday = today.format('MM-DD') === birthDate.format('MM-DD');
+
+  if (isBirthdayToday) {
+    alert('Wszystkiego najlepszego!');
+  }
+
+  dialogContent.textContent = `Od Twoich narodzin minęło dokładnie ${daysPassed} dni.`;
+  
+  dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+});
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // Pamiętaj, żeby tu wpisać nazwę swojego repozytorium z zadania!
   base: '/projektWebowe/', 
   plugins: [
     tailwindcss(),
